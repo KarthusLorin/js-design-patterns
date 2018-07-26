5a574e3a2cd66f4215ffb7ea07dd804a590fbb3d
KarthusLorin <karthuslorin@gmail.com>
2018-07-26 23:04:35 +0800

    init project

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
     filename: './release/bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    // 根目录
+    contentBase: path.join(__dirname, './release'),
+    // 自动打开浏览器
+    open: true,
+    port: 9000
   }
 }
