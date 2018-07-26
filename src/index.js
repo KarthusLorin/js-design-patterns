f5513ea5e0372ec85d4e3cec05966b708b2a1fb1
KarthusLorin <karthuslorin@gmail.com>
2018-07-26 23:19:46 +0800

    init project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert(1)
+class Person {
+  constructor (name) {
+    this.name = name
+  }
+  getName () {
+    return this.name
+  }
+}
+
+let p = new Person('lorin')
+
+console.log(p.getName())
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -7,6 +7,13 @@ module.exports = {
     path: __dirname,
     filename: './release/bundle.js'
   },
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader'
+    }]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
